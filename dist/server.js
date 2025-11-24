23a6866e2789f62f56a7f78d12b4382abf51c023
unknown <tarlison.dev@gmail.com>
2025-11-24 08:54:33 -0300

    update config cors

--- dist/server.js
+++ dist/server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors({
-    origin: ['https://id-preview--d5c562a4-a3ba-4bcb-b52a-bd66c311853e.lovable.app', 'http://localhost:3000', '*'],
+    origin: ['*'],
     methods: ['GET', 'POST'],
-    credentials: true
 }));
 app.use('/api/v1', route);
 app.listen(config.port, () => console.info(`http://localhost:${config.port}`));
