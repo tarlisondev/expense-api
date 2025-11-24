eb4d5feafd73f68a398946cd400dd55c885f5368
unknown <tarlison.dev@gmail.com>
2025-11-24 08:40:19 -0300

    configure cors

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,11 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(helmet());
-app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
+app.use(cors({
+    origin: ['https://id-preview--d5c562a4-a3ba-4bcb-b52a-bd66c311853e.lovable.app', 'http://localhost:3000', '*'],
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 app.use('/api/v1', route);
 app.listen(config.port, () => console.info(`http://localhost:${config.port}`));
 //# sourceMappingURL=server.js.map
